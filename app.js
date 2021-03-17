a7003f4c6b92b9b7330d49379909ecf9d21b24cd
3fon <threefonoff@gmail.com>
2021-03-17 15:22:08 +0700

    Add MongoDB

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const https = require('https');
+const mongoose = require('mongoose')
 const date = require(__dirname + "/date.js");
+
 const day = date.getDay();
 
 const app = express();
 
 let list = "/"
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = [];
 let workItems = [];
+
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+
+const itemsSchema = {
+    name: String,
+    list: String,
+}
+
+const Item = mongoose.model("Item", itemsSchema);
 
 
 
@@ -19,8 +31,16 @@ app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {
-    list = "/work";
-    res.render("list", {listTitle: day, newListItem: items, whichList: list})
+    list = "/";
+    Item.find({list: "main"},(err, foundItems) =>{
+        if (err) {
+            console.log(err);
+        } else {
+
+            res.render("list", {listTitle: day, newListItem: foundItems, whichList: list})
+        }
+    });
+
 })
 
 app.post("/", (req, res) => {
@@ -48,6 +68,5 @@ app.get("/about", (req, res) => {
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 })
-
 
 
