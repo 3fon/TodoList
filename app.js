5eb06e50e93e765a9b48d53ac469646b978da3d2
3fon <threefonoff@gmail.com>
2021-03-17 19:21:48 +0700

    Add custom list handler

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const https = require('https');
 const mongoose = require('mongoose')
 const date = require(__dirname + "/date.js");
-
+const _ = require('lodash');
 
 const day = date.getDay();
 const app = express();
@@ -14,12 +14,16 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
+    name: String
+}
+
+const listSchema = {
     name: String,
-    list: String,
+    items: [itemsSchema],
 }
 
 const Item = mongoose.model("Item", itemsSchema);
-
+const List = mongoose.model("List", listSchema);
 
 
 app.set('view engine', 'ejs');
@@ -29,27 +33,8 @@ app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {
-    list = "/";
-    Item.find({list: "main"},(err, foundItems) =>{
-        if (err) {
-            console.log(err);
-        } else {
-
-            res.render("list", {listTitle: day, newListItem: foundItems, whichList: list})
-        }
-    });
-
-});
-
-app.get("/work", (req, res) => {
-    list = "/work";
-    Item.find({list: "work"},(err, foundItems) =>{
-        if (err) {
-            console.log(err);
-        } else {
-
-            res.render("list", {listTitle: day, newListItem: foundItems, whichList: list})
-        }
+    List.findOne({name: "Today"}, (err, foundList) => {
+        res.render("list", {listTitle: "Today", newListItem: foundList.items, whichList: list})
     });
 });
 
@@ -57,41 +42,61 @@ app.get("/about", (req, res) => {
     res.render("about");
 });
 
-// POSTS
-
-app.post("/", (req, res) => {
-    const item = new Item ({
-        name: req.body.newItem,
-        list: "main"
-    });
-    item.save();
-    res.redirect("/");
-});
-
-app.post("/work", (req, res) => {
-    const item = new Item ({
-        name: req.body.newItem,
-        list: "work"
-    });
-    item.save();
-    res.redirect("/work");
-});
-
-app.post("/delete", (req, res) => {
-
-    const checkedItemId = req.body.checkedItem;
-    Item.findOne({_id: checkedItemId}, (err, foundItem) => {
-
-        if(err){
+app.get("/:customListName", (req, res) => {
+    const listName = _.capitalize(req.params.customListName);
+    List.findOne({name: listName}, (err, foundList) => {
+        if (err) {
             console.log(err);
+        } else if (!foundList) {
+            // if foundList doesnt exists - create a new list
+            console.log("Creating New list \n");
+            const list = new List({
+                name: listName,
+            });
+            list.save();
+            console.log("redirecting \n");
+            res.redirect("/" + listName);
         } else {
-            foundItem.delete();
-            res.redirect("/" + foundItem.list);
+            res.render("list", {listTitle: listName, newListItem: foundList.items, whichList: listName})
         }
     });
 });
 
 
+
+// POSTS
+
+app.post("/", (req, res) => {
+    const itemName = req.body.newItem;
+    const listName = req.body.listName;
+    const item = new Item ({
+        name: itemName,
+    });
+
+    List.findOne({name: listName}, (err, foundList) => {
+        foundList.items.push(item);
+        foundList.save();
+        if (listName === "Today"){
+            res.redirect("/");
+        } else {
+            res.redirect("/" + listName);
+        }
+    });
+
+});
+
+app.post("/delete", (req, res) => {
+    const itemId = req.body.checkedItem;
+    const listName = req.body.listName;
+
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: itemId}}}, (err, foundList) => {
+        if (err){
+            console.log(err);
+        } else {
+            res.redirect("/" + listName);
+        }
+    });
+});
 
 
 app.listen(3000, () => {
