d6703996a531795de59de99098eb0f225963d6cb
3fon <threefonoff@gmail.com>
2021-03-17 20:26:26 +0700

    Set up Heroku

--- app.js
+++ app.js
@@ -99,8 +99,8 @@ app.post("/delete", (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started successfully!");
 });
 
 
