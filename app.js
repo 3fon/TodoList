7dc3b786f23da37e75ba6c1750e58f5b123aa402
3fon <threefonoff@gmail.com>
2021-03-17 16:49:57 +0700

    Set up Delete and Add for all lists

--- app.js
+++ app.js
@@ -4,13 +4,11 @@ const https = require('https');
 const mongoose = require('mongoose')
 const date = require(__dirname + "/date.js");
 
-const day = date.getDay();
 
+const day = date.getDay();
 const app = express();
 
-let list = "/"
-let items = [];
-let workItems = [];
+let list = "/";
 
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
@@ -26,7 +24,7 @@ const Item = mongoose.model("Item", itemsSchema);
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
@@ -41,32 +39,63 @@ app.get("/", (req, res) => {
         }
     });
 
-})
-
-app.post("/", (req, res) => {
-    let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
-})
+});
 
 app.get("/work", (req, res) => {
     list = "/work";
-    res.render("list", {listTitle: "Work List", newListItem: workItems, whichList: list});
-})
+    Item.find({list: "work"},(err, foundItems) =>{
+        if (err) {
+            console.log(err);
+        } else {
 
-app.post("/work", (req, res) => {
-    let item = req.body.newItem;
-    workItems.push(item);
-    res.redirect("/work");
-})
+            res.render("list", {listTitle: day, newListItem: foundItems, whichList: list})
+        }
+    });
+});
 
 app.get("/about", (req, res) => {
     res.render("about");
-})
+});
+
+// POSTS
+
+app.post("/", (req, res) => {
+    const item = new Item ({
+        name: req.body.newItem,
+        list: "main"
+    });
+    item.save();
+    res.redirect("/");
+});
+
+app.post("/work", (req, res) => {
+    const item = new Item ({
+        name: req.body.newItem,
+        list: "work"
+    });
+    item.save();
+    res.redirect("/work");
+});
+
+app.post("/delete", (req, res) => {
+
+    const checkedItemId = req.body.checkedItem;
+    Item.findOne({_id: checkedItemId}, (err, foundItem) => {
+
+        if(err){
+            console.log(err);
+        } else {
+            foundItem.delete();
+            res.redirect("/" + foundItem.list);
+        }
+    });
+});
+
+
 
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
-})
+});
 
 
